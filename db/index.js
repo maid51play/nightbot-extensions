9e735897214f6f8235047091c315565b01752970
MeganeMidori <MeganeMidori@users.noreply.github.com>
2019-09-23 19:24:48 -0700

    connection string?

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({connectionString: process.env.DB_CONNECTION_STRING})
 module.exports = {
   query: (text, params) => pool.query(text, params),
 }
